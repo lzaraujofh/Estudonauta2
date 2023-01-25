be61ef3badb534015ce671d8452a23c51a74c476
Luiz Araújo <araujofh58@gmail.com>
2023-01-24 22:27:04 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<!-- Luiz, 13/11/22 -->
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listagem de Jogos</title>
+    <link rel="stylesheet" href="estilos/estilo.css">
+
+</head>
+<body>
+    <main id="corpo">
+        <h1>Escolha seu Jogo</h1>
+        <table>
+
+
+        
+        </table>
+
+
+    </main>
+</body>
+</html>
